195e7c8ed036e43144621fd55a3296fb5074c52c
Shotaro Tsuji <Shotaro.Tsuji@gmail.com>
2021-09-25 00:51:12 +0900

    Add mod file

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -7,19 +7,43 @@ use nom::{
     sequence::tuple,
 };
 
+#[derive(Debug)]
 pub enum LedgerItem<'a> {
     Transaction(transaction::Transaction<'a>),
+    Blank,
 }
 
 pub struct LedgerParser<'a> {
     s: &'a str,
 }
 
+impl<'a> LedgerParser<'a> {
+    pub fn new(s: &'a str) -> Self {
+        Self {
+            s: s,
+        }
+    }
+}
+
 impl<'a> Iterator for LedgerParser<'a> {
     type Item = LedgerItem<'a>;
 
     fn next(&mut self) -> Option<LedgerItem<'a>> {
-        None
+        if self.s.is_empty() {
+            return None;
+        }
+
+        let (remain, ret) = if self.s.starts_with(|c: char| c.is_ascii_digit()) {
+            let t = transaction::transaction(self.s).unwrap();
+            (t.0, LedgerItem::Transaction(t.1))
+        } else {
+            let (remain, _) = blank_line(self.s).unwrap();
+            (remain, LedgerItem::Blank)
+        };
+
+        self.s = remain;
+
+        Some(ret)
     }
 }
 
